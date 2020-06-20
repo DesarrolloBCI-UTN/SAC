a62c9e8b166f5406bc6be904de8e1411368855d9
Germán Castro <6castrogerman@gmail.com>
2020-06-20 18:21:44 -0300

    Ejemplo basico QPushButton->setIcon

--- example/mainwindow.cpp
+++ example/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPushButton>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,10 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+   QString a = ":/Img/flecha.png";
+   ui->pushButton->setIcon(QIcon(a));
+   ui->pushButton->setText(a);
+}
